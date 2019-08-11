64864de736d8002fc1395ce3e5a9a7c705931cb7
L1berato <juliano.cal@gmail.com>
2019-08-11 19:32:28 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
-# Create JulianoL1berato
-#
-# Desabilita selinux:
+## Create JulianoL1berato
+##
+## disable selinux:
 setenforce 0
 sudo sed -i "s/enforcing/disabled/g" /etc/selinux/config
-# Updates
+## updates
 yum update -y 
 yum upgrade -y
-# Install Utilities
+## Install Utilities
 yum install vim unzip openssh-server -y
 
-# Change config sshd
+## change config sshd
 cat /etc/ssh/sshd_config | grep -v "PermitRootLogin" >
 cat /etc/ssh/sshd_config | grep -v "PasswordAuthentication" >
 echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 systemctl restart sshd
 
-# Change configure Vim
+## flush rules
+iptables −F
+iptables −X
+iptables −Z
+iptables −t nat −F
+## initial rules
+iptables −P INPUT ACCEPT
+iptables −P OUTPUT ACCEPT
+iptables −P FORWARD ACCEPT
+iptables −t nat −P PREROUTING ACCEPT
+iptables −t nat −P POSTROUTING ACCEPT
+iptables −A INPUT −i lo −j ACCEPT
+
+
+## change configure vim
 echo "colorscheme elflord" > ~/.vimrc
 echo "syntax on" >> ~/.vimrc
