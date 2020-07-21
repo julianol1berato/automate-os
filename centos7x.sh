89747e8df5a8b5bd5dba9e4648fc2c40e37ec2c9
L1berato <juliano.cal@gmail.com>
2020-07-21 18:25:42 -0300

    Update centos7x.sh

--- centos7x.sh
+++ centos7x.sh
@@ -21,6 +21,15 @@ echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 echo 'HISTTIMEFORMAT="%d/%m/%Y - %T => "' >> ~/.bashrc
 echo "PS1='\[\033[00;32m\]┌─[\[\033[00;31m\]\u\[\033[00m\]@\[\033[01;34m\]\h\[\033[00;32m\]]──[\[\033[01;33m\]\A\[\033[00;32m\]]──[\[\033[00m\]\w\[\033[00;32m\]]\n└─[\!]─>\$\[\033[00;32m\] '" >> ~/.bashrc
+# remember the last 1000 commands in history
+echo "HISTSIZE=50000" >> ~/.bashrc
+# Limit the size of the ~/.bash_history file
+echo "HISTFILESIZE=100000" >> ~/.bashrc
+# append to the history file, don't overwrite it
+echo "shopt -s histappend" >> ~/.bashrc
+# Avoid duplicate entries
+echo "export HISTCONTROL=ignoredups:erasedups" >> ~/.bashrc
+
 systemctl restart sshd
 
 ## flush rules
