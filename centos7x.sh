e1a24158ac374812d7c948b34822d227b9beba1a
L1berato <juliano.cal@gmail.com>
2020-09-01 16:07:22 -0300

    Update centos7x.sh

--- centos7x.sh
+++ centos7x.sh
@@ -1,48 +1,36 @@
 #!/bin/bash
-## Create JulianoL1berato
+## Create JulianoL1berato - https://zbx.liberato.dev
 ## CentOS 7.x
 ## disable selinux:
 setenforce 0
 sudo sed -i "s/enforcing/disabled/g" /etc/selinux/config
-## updates
 yum update -y 
 yum upgrade -y
-
-## Install Utilities
-yum install vim unzip openssh-server wget telnet epel-release -y
-#sysadmin? 
-#yum install bind-utils nmap 
-
+yum install vim unzip openssh-server bind-utils wget telnet net-snmp-utils nmap epel-release -y
 
 ## change config sshd
 cp /etc/ssh/sshd_config{,.original}
 echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 echo 'HISTTIMEFORMAT="%d/%m/%Y - %T => "' >> ~/.bashrc
-echo "PS1='\[\033[00;32m\]┌─[\[\033[00;31m\]\u\[\033[00m\]@\[\033[01;34m\]\h\[\033[00;32m\]]──[\[\033[01;33m\]\A\[\033[00;32m\]]──[\[\033[00m\]\w\[\033[00;32m\]]\n└─[\!]─>\$\[\033[00;32m\] '" >> ~/.bashrc
-# remember the last 1000 commands in history
 echo "HISTSIZE=50000" >> ~/.bashrc
-# Limit the size of the ~/.bash_history file
 echo "HISTFILESIZE=100000" >> ~/.bashrc
 # append to the history file, don't overwrite it
 echo "shopt -s histappend" >> ~/.bashrc
-# Avoid duplicate entries
+# avoid duplicate entries
 echo "export HISTCONTROL=ignoredups:erasedups" >> ~/.bashrc
-
 systemctl restart sshd
-
 timedatectl set-timezone America/Sao_Paulo
-
 
 ## flush rules
 /usr/sbin/iptables −F
 /usr/sbin/iptables −X
 /usr/sbin/iptables −Z
 ## initial rules
-iptables −P INPUT ACCEPT
-iptables −P OUTPUT ACCEPT
-iptables −P FORWARD ACCEPT
-iptables −A INPUT −i lo −j ACCEPT
+/usr/sbin/iptables −P INPUT ACCEPT
+/usr/sbin/iptables −P OUTPUT ACCEPT
+/usr/sbin/iptables −P FORWARD ACCEPT
+/usr/sbin/iptables −A INPUT −i lo −j ACCEPT
 systemctl stop firewalld
 systemctl disable firewalld
 
@@ -50,4 +38,7 @@ systemctl disable firewalld
 echo "colorscheme elflord" > ~/.vimrc
 echo "syntax on" >> ~/.vimrc
 bash
+
+## custom
+##echo "PS1='\[\033[00;32m\]┌─[\[\033[00;31m\]\u\[\033[00m\]@\[\033[01;34m\]\h\[\033[00;32m\]]──[\[\033[01;33m\]\A\[\033[00;32m\]]──[\[\033[00m\]\w\[\033[00;32m\]]\n└─[\!]─>\$\[\033[00;32m\] '" >> ~/.bashrc
 
