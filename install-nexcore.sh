f7a12f73a70e68b2ef53168f14a1647f53213ecc
L1berato <juliano@liberato.eti.br>
2021-10-04 09:41:35 -0300

    Update install-nexcore.sh

--- install-nexcore.sh
+++ install-nexcore.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-
-apt update -y && apt upgrade -y && apt install wget -y && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt-get install -f
+apt update -y && apt upgrade -y && apt install wget -y && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && dpkg -i google-chrome-stable_current_amd64.deb && apt-get install -f
