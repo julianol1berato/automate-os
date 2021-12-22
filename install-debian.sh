91baac0eccdbe409bc7ec6a3a016d7421304805c
L1berato <juliano@liberato.eti.br>
2021-12-22 15:07:54 -0300

    Update install-debian.sh

--- install-debian.sh
+++ install-debian.sh
@@ -2,5 +2,19 @@
 ## julianol1berato - projects debian
 ## setup initial
 
+## if UPDATE 
 apt update && apt upgrade -y
-apt install wget curl vim atop htop nmap 
+apt install wget curl vim atop htop nmap whowatch mtr htop rsync acl locate rcconf ethtool screen lshw hdparm ttyrec tcpdump auditd sudo mpg123 sox make gcc g++ openssh-server libsox-fmt-base dialog unzip autoconf -y
+
+
+
+## if 
+
+echo 'HISTTIMEFORMAT="%d/%m/%Y - %T => "' >> ~/.bashrc
+echo "HISTSIZE=50000" >> ~/.bashrc
+echo "HISTFILESIZE=100000" >> ~/.bashrc
+echo "shopt -s histappend" >> ~/.bashrc
+echo "export HISTCONTROL=ignoredups:erasedups" >> ~/.bashrc
+cp ~/.bashrc{,.orig}
+echo "PS1='\[\033[00;32m\]┌─[\[\033[00;31m\]\u\[\033[00m\]@\[\033[01;34m\]\h\[\033[00;32m\]]──[\[\033[01;33m\]\A\[\033[00;32m\]]──[\[\033[00m\]\w\[\033[00;32m\]]\n└─[\!]─>\$\[\033[00;32m\] '" >> ~/.bashrc
+
