668fc9ceae34e6670488a9466ed118288effa103
Juliano Liberato <juliano@9level.com.br>
2024-08-08 21:08:16 -0300

    Delete install-googlechrome.sh

--- install-googlechrome.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-apt update -y && apt upgrade -y && apt install wget -y && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && dpkg -i google-chrome-stable_current_amd64.deb && apt-get install -f
